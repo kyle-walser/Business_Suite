0444e9edaf7a1aef249527cbe553fa62c156d967
kyle-walser <kyle.walser@hotmail.com>
2017-07-09 00:38:50 -0400

    update 001

--- src/Security/eBay_Shipping.java
+++ src/Security/eBay_Shipping.java
@@ -13,8 +13,15 @@ import javax.swing.JButton;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JCheckBox;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class eBay_Shipping extends JFrame {
 
@@ -24,7 +31,8 @@ public class eBay_Shipping extends JFrame {
 	private JTextField txtSoldAmt;
 	private JTextField txtShippedAmt;
 	private JTextField txtPaypalAmt;
-	private JTextField txtDateSold;
+	private JTextField txtDteSld;
+	private JTextField txtDteShp;
 
 	/**
 	 * Launch the application.
@@ -81,33 +89,16 @@ public class eBay_Shipping extends JFrame {
 		txtTitle.setEnabled(false);
 		txtTitle.setEditable(false);
 		txtTitle.setVisible(false);
-		txtTitle.setBounds(69, 65, 280, 20);
+		txtTitle.setBounds(115, 65, 268, 20);
 		contentPane.add(txtTitle);
 		txtTitle.setColumns(10);
 		
-		JComboBox cmboUSPSShipOpt = new JComboBox();
-		cmboUSPSShipOpt.setModel(new DefaultComboBoxModel(new String[] {"First-Class", "Priority(W)", "Priority(FRE)", "Priority(FRPE)", "Priority(FRS)", "Priority(FRM)", "Priority(FRL)", "Express"}));
-		cmboUSPSShipOpt.setBounds(297, 115, 89, 20);
-		cmboUSPSShipOpt.setVisible(false);
-		contentPane.add(cmboUSPSShipOpt);
+		JComboBox cmboShipOpt = new JComboBox();
+		cmboShipOpt.setBounds(297, 115, 89, 20);
+		cmboShipOpt.setVisible(false);
+		contentPane.add(cmboShipOpt);
 		
-		JComboBox cmboShipService = new JComboBox();
-		cmboShipService.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String ship = (String) cmboShipService.getSelectedItem();
-				
-				if (ship == "USPS")
-				{
-					cmboUSPSShipOpt.setVisible(true);
-					//cmboUPSShipOpt.setVisible(false);
-				}
-						
-			}
-		});
-		cmboShipService.setModel(new DefaultComboBoxModel(new String[] {"USPS", "UPS"}));
-		cmboShipService.setVisible(false);
-		cmboShipService.setBounds(118, 115, 66, 20);
-		contentPane.add(cmboShipService);
+		
 		
 		JLabel lblShippingService = new JLabel("Shipping Service:");
 		lblShippingService.setToolTipText("Shipping Service used");
@@ -116,57 +107,172 @@ public class eBay_Shipping extends JFrame {
 		contentPane.add(lblShippingService);
 		
 		JLabel lblSoldAmount = new JLabel("Sold Amount:");
+		lblSoldAmount.setVisible(false);
 		lblSoldAmount.setBounds(10, 93, 86, 14);
 		contentPane.add(lblSoldAmount);
 		
 		txtSoldAmt = new JTextField();
-		txtSoldAmt.setBounds(98, 90, 86, 20);
+		txtSoldAmt.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				if (txtSoldAmt.getText().length() > 0){
+					txtPaypalAmt.setText(Double.toString((Double.parseDouble(txtSoldAmt.getText())*.1)));
+					
+				}
+			}
+		});
+		txtSoldAmt.setBounds(115, 90, 86, 20);
+		txtSoldAmt.setVisible(false);
 		contentPane.add(txtSoldAmt);
 		txtSoldAmt.setColumns(10);
 		
 		JLabel lblShippedPrice = new JLabel("Shipped Price:");
 		lblShippedPrice.setBounds(10, 146, 86, 14);
+		lblShippedPrice.setVisible(false);
 		contentPane.add(lblShippedPrice);
 		
 		txtShippedAmt = new JTextField();
-		txtShippedAmt.setBounds(98, 143, 86, 20);
+		txtShippedAmt.setBounds(115, 143, 86, 20);
+		txtShippedAmt.setVisible(false);
 		contentPane.add(txtShippedAmt);
 		txtShippedAmt.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("Paypal Fee:");
+		lblNewLabel_2.setVisible(false);
 		lblNewLabel_2.setBounds(10, 171, 86, 14);
 		contentPane.add(lblNewLabel_2);
 		
 		txtPaypalAmt = new JTextField();
-		txtPaypalAmt.setBounds(98, 168, 86, 20);
+		txtPaypalAmt.setBounds(115, 168, 86, 20);
+		txtPaypalAmt.setVisible(false);
+		txtPaypalAmt.setEditable(false);
 		contentPane.add(txtPaypalAmt);
 		txtPaypalAmt.setColumns(10);
 		
-		txtDateSold = new JTextField();
-		txtDateSold.setBounds(98, 196, 86, 20);
-		contentPane.add(txtDateSold);
-		txtDateSold.setColumns(10);
+		txtDteSld = new JTextField();
+		txtDteSld.setBounds(297, 90, 86, 20);
+		txtDteSld.setVisible(false);
+		contentPane.add(txtDteSld);
+		txtDteSld.setColumns(10);
 		
 		JLabel lblNewLabel_3 = new JLabel("Date Sold:");
-		lblNewLabel_3.setBounds(10, 199, 86, 14);
+		lblNewLabel_3.setVisible(false);
+		lblNewLabel_3.setBounds(211, 93, 73, 14);
 		contentPane.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel_4 = new JLabel("Date Shipped:");
-		lblNewLabel_4.setBounds(10, 224, 46, 14);
+		lblNewLabel_4.setVisible(false);
+		lblNewLabel_4.setBounds(211, 146, 76, 14);
 		contentPane.add(lblNewLabel_4);
 		
 		JLabel lblNewLabel_5 = new JLabel("Shipping Opt.:");
-		lblNewLabel_5.setBounds(223, 118, 73, 14);
+		lblNewLabel_5.setVisible(false);
+		lblNewLabel_5.setBounds(211, 118, 86, 14);
 		contentPane.add(lblNewLabel_5);
+		
+		JComboBox cmboShipService = new JComboBox();
+		cmboShipService.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			//	String ship = (String) cmboShipService.getSelectedItem();
+				
+				cmboShipOpt.setEnabled(true);
+				if ((String) cmboShipService.getSelectedItem() == "USPS")
+				{
+					
+					cmboShipOpt.setModel(new DefaultComboBoxModel(new String[] {"First-Class", "Priority(W)", "Priority(FRE)", "Priority(FRPE)", "Priority(FRS)", "Priority(FRM)", "Priority(FRL)", "Express"}));
+				
+				}else if((String) cmboShipService.getSelectedItem() == "UPS"){
+					
+					cmboShipOpt.setModel(new DefaultComboBoxModel(new String[] {"ground", "Air"}));
+				
+				}else{
+					cmboShipOpt.setModel(new DefaultComboBoxModel(new String[] {""}));
+					cmboShipOpt.setEnabled(false);;
+					
+				}
+				
+				
+						
+			}
+		});
+		cmboShipService.setModel(new DefaultComboBoxModel(new String[] {"Choose", "USPS", "UPS"}));
+		cmboShipService.setVisible(false);
+		
+		cmboShipService.setBounds(115, 115, 86, 20);
+		contentPane.add(cmboShipService);
+		
+		txtDteShp = new JTextField();
+		txtDteShp.setBounds(297, 143, 86, 20);
+		txtDteShp.setVisible(false);
+		contentPane.add(txtDteShp);
+		txtDteShp.setColumns(10);
+		
+		
+		
+		
+		JCheckBox chkSldDte = new JCheckBox("Today?");
+		chkSldDte.setVisible(false);
+		chkSldDte.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (chkSldDte.isSelected() == true){
+					txtDteSld.setText(getDate());
+					txtDteSld.setEditable(false);
+					
+				}else{
+					txtDteSld.setText("");
+					txtDteSld.setEditable(true);
+				}
+			}
+		});
+		chkSldDte.setBounds(389, 89, 97, 23);
+		contentPane.add(chkSldDte);
+		
+		JCheckBox chkDteShp = new JCheckBox("Today ?");
+		chkDteShp.setVisible(false);
+		chkDteShp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (chkDteShp.isSelected() == true){
+					txtDteShp.setText(getDate());
+					txtDteShp.setEditable(false);
+					
+				}else{
+					txtDteShp.setText("");
+					txtDteShp.setEditable(true);
+				}
+			}
+		});
+		chkDteShp.setBounds(389, 142, 97, 23);
+		contentPane.add(chkDteShp);
 		
 		btnSearchItem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				if (txtEbayID.getText().length() >  0 && txtEbayID.getText().substring(0, 2).toUpperCase().equals("EB") == true){
-					
+					txtEbayID.setText(txtEbayID.getText().toUpperCase());
+					txtPaypalAmt.setVisible(true);
+					chkSldDte.setVisible(true);
 					btnSearchItem.setEnabled(false);
 					txtEbayID.setEnabled(false);
 					lblNewLabel_1.setVisible(true);
 					cmboShipService.setVisible(true);
+					txtTitle.setVisible(true);
+					lblSoldAmount.setVisible(true);
+					lblNewLabel_4.setVisible(true);
+					txtPaypalAmt.setVisible(true);
+					lblShippingService.setVisible(true);
+					chkDteShp.setVisible(true);
+					txtDteShp.setVisible(true);
+					lblNewLabel_3.setVisible(true);
+					txtSoldAmt.setVisible(true);
+					txtDteSld.setVisible(true);
+					lblNewLabel_5.setVisible(true);
+					cmboShipOpt.setVisible(true);
+					lblShippedPrice.setVisible(true);
+					txtShippedAmt.setVisible(true);
+					lblNewLabel_2.setVisible(true);
+				
+					
+					
+					
 				}else{
 					JOptionPane.showMessageDialog(null, "You have entered an Invalid ID. \n Please try again", "Invaild ID Error", JOptionPane.ERROR_MESSAGE);
 					txtEbayID.setText("");
@@ -176,6 +282,9 @@ public class eBay_Shipping extends JFrame {
 			}
 		});
 	}
-    /** Listens to the combo box. */
-   
+	private String getDate(){
+		DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+		Date date = new Date();
+		return dateFormat.format(date);
+	}
 }
